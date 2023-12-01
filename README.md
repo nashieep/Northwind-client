013b4617c831e8cb0c9e30e2b69d6e4c3d7be753
nashieep <nmpongnon@gmail.com>
2023-12-01 14:31:43 -0600

    northwind client

---
+++ README.md
@@ -0,0 +1,38 @@
+# Northwind Store Web Application
+
+Welcome to the Northwind Store Web Application! This project is a simple web application that allows users to search for and view details of products from the Northwind database. The application is built using HTML, CSS, and JavaScript, with dynamic content fetching from a RESTful API.
+
+## Table of Contents
+- [Overview](#overview)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Overview
+
+The Northwind Store Web Application provides a user-friendly interface to interact with the Northwind database, showcasing various features:
+
+- **Search by Category:** Users can search for products based on categories.
+- **View All Products:** Users have the option to view all products available.
+- **Product Details:** Detailed information about each product, including product ID, name, unit price, and more.
+- **Dynamic Image Display:** Category images are dynamically incorporated into the page based on user interactions.
+
+## Features
+
+1. **Search by Category:**
+   - Select a category from the dropdown to filter products by category.
+   - Category images are discreetly displayed on the page.
+
+2. **View All Products:**
+   - Choose the "View All" option to see a list of all available products.
+   - Products are sorted alphabetically by name.
+
+3. **Product Details:**
+   - Click on a specific product to view detailed information.
+   - Navigate to the product details page for additional details.
+
+4. **Dynamic Image Display:**
+   - Category images are discreetly incorporated into the page based on the selected category.
+
---
+++ scripts/search.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", e => {
+
+    mode.addEventListener("change", e => {
+        if (mode.value === "") {
+            //HIDE ALL
+            catList.classList.add("hidden")
+            prodList.classList.add("hidden")
+        } else if (mode.value === "all") {
+            //HIDE CATEGORY
+            catList.classList.add("hidden")
+            //SHOW PROD LIST
+            prodList.classList.remove("hidden")
+            fetch("http://localhost:8081/api/products/")
+            .then(r => r.json())
+            .then(data => {
+                data.sort((a,b) => a.productName > b.productName ? 1 : -1);//EXTRA CREDIT SORT
+                prodList.innerHTML = "<option value=\"\">Select a Product</option>"; //CLEAR PROD LIST
+                data.forEach(item => {
+                    prodList.innerHTML += `<option value="${item.productId}">${item.productName} - $${Number(item.unitPrice).toFixed(2)}</option>`
+                })
+            })
+        } else {
+            //SHOW CATEGORY
+            catList.classList.remove("hidden")
+            prodList.classList.remove("hidden")
+        }
+    });
+
+    fetch("http://127.0.0.1:8081/api/categories")
+        .then(r => r.json())
+        .then(data => {
+            data.forEach(item => catList.innerHTML += `<option value="${item.categoryId}">${item.name} - ${item.description}</option>`)
+        })
+
+    catList.addEventListener("change", e => {
+        fetch("http://localhost:8081/api/products/")
+            .then(r => r.json())
+            .then(data => {
+                data.sort((a,b) => a.productName > b.productName ? 1 : -1);//EXTRA CREDIT SORT
+                prodList.innerHTML = "<option value=\"\">Select a Product</option>"; //CLEAR PROD LIST
+                productImage.src = `./images/cat${catList.value}.png`;
+                data.filter(i => i.categoryId === catList.value).forEach(item => {
+                    prodList.innerHTML += `<option value="${item.productId}">${item.productName} - $${Number(item.unitPrice).toFixed(2)}</option>`
+                })
+            })
+    })
+
+
+    prodList.addEventListener("change", e => {
+        fetch("http://localhost:8081/api/products/" + prodList.value)
+        .then(r => r.json())
+        .then(item => {
+            details.innerHTML = ""; //CLEAR 
+            details.innerHTML += `<tr><th>product Id:</th><td>${item.productId}</td></tr>`
+            details.innerHTML += `<tr><th>product Name:</th><td>${item.productName}</td></tr>`
+            details.innerHTML += `<tr><th>unit Price:</th><td>$${Number(item.unitPrice).toFixed(2)}</td></tr>`
+            details.innerHTML += `<tr><th>Link to details:</th><td>
+                <a href="details.html?productId=${item.productId}">
+                    <button> Details </button>
+                </a>
+            </td></tr>`
+        })
+        // window.location = "details.html?productId=" + prodList.value;
+    })
+
+}) //END DOM CONTENT LOADED
